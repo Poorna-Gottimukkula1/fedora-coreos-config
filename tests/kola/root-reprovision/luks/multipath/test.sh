0f59533cf515f081dc0fac749611c7b673884e40
Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>
2024-08-28 12:09:20 +0530

    Update test.sh
    Signed-off-by: Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>

--- tests/kola/root-reprovision/luks/multipath/test.sh
+++ tests/kola/root-reprovision/luks/multipath/test.sh
@@ -13,11 +13,31 @@
 ##   timeoutMin: 15
 ##   description: Verify that LUKS on a mpath disks works.
 ##   primaryDisk: ":mpath"
+##   appendKernelArgs: "rd.multiath=default"
 
 set -xeuo pipefail
 
 # shellcheck disable=SC1091
 . "$KOLA_EXT_DATA/commonlib.sh"
 
+# Check that the LUKS device parent is a multipath device
+
+# Replace 'luksroot' with the actual LUKS device name if different
+srcdev=$(findmnt -nvr /sysroot -o SOURCE)
+#exp_parent="/dev/mapper/mpatha4"
+
+# Get the parent device of the LUKS device
+#parent_device=$(lsblk -no pkname "$srcdev")
+
+----
+# Variables
+#child_device="/dev/mapper/root"
+parent_device="/dev/mapper/mpatha4"
+
+# Check if the child device is part of the parent device
+if ! lsblk -pno NAME "$parent_device" | grep -qw "$srcdev"; then
+    fatal "$child_device is NOT a child of $parent_device."
+fi
+ok "$child_device device is part of the parent $parent_device device"
 # run the rest of the tests
 . $KOLA_EXT_DATA/luks-test.sh
